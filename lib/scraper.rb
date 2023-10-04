860c421648be0a2149b0da4f28cdbd8a375d04b8
alvinkenyagah <alvin47k@gmail.com>
2023-10-04 18:16:38 +0300

    Example of scrapping h2 content from wikipedia

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# URL of the Wikipedia page
+url = 'https://en.wikipedia.org/wiki/Web_scraping'
+
+# Fetch the web page's HTML content
+html_content = URI.open(url)
+
+# Parse the HTML with Nokogiri
+doc = Nokogiri::HTML(html_content)
+
+# Select all <h2> elements on the page
+h2_elements = doc.css('h2')
+
+# Loop through the <h2> elements and print their text content
+h2_elements.each do |h2|
+  puts h2.text
+end
